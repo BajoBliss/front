dfcdd7d5f531dea23cdcf15e05153b53af1d4f52
hasyim404 <muhaisim7@gmail.com>
2025-03-01 00:04:09 +0700

    init app-routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,73 @@
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Faq from "../pages/Faq";
+
+const Routes = () => {
+  // Define public routes accessible to all users
+  const routesForPublic = [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    // {
+    //   path: "/home",
+    //   element: <Home />,
+    // },
+    {
+      path: "/products",
+      element: <Products />,
+    },
+    {
+      path: "/faq",
+      element: <Faq />,
+    },
+  ];
+
+  // Define routes accessible only to authenticated users
+  //    const routesForAuthenticatedOnly = [
+  //     {
+  //       path: "/admin/",
+  //     //   element: redirect dashboard or 404 ,
+  //     },
+  //     {
+  //       path: "/admin/*",
+  //       element: <ProtectedRoute />, // Wrap the component in ProtectedRoute
+  //       children: [
+  //         {
+  //           path: "dashboard",
+  //           element: <Dashboard />,
+  //         },
+  //       ],
+  //     },
+  //   ];
+
+  // Define routes accessible only to non-authenticated users
+
+  const routesForNotAuthenticatedOnly = [
+    {
+      path: "/register",
+      element: "register",
+    },
+    {
+      path: "/login",
+      element: "login",
+    },
+  ];
+
+  // Combine and conditionally include routes based on authentication status
+  const router = createBrowserRouter([
+    ...routesForPublic,
+    ...routesForNotAuthenticatedOnly,
+    // ...(!token ? routesForNotAuthenticatedOnly : []),
+    // ...routesForAuthenticatedOnly,
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Routes;
